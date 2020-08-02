c6060db31e79a0feb98aab93fbf9bfae93834965
Mauricio Fullana <mauricio@bankingly.com>
2020-08-02 00:19:34 -0300

    authRoutes format Movements response fix add .prettierrc file

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -8,84 +8,77 @@ var constants = require('../models/Constants');
 const router = express.Router();
 
 router.post('/signup', async (req, res) => {
-    
-    try {
-        const user = new User({ 
-            name, 
-            lastName, 
-            email, 
-            userName,
-            password
-        } = req.body);
+	try {
+		const user = new User(
+			({ name, lastName, email, userName, password } = req.body)
+		);
 
-        await user.save();
-    
-        const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-        delete user.password;
-        res.send({
-            status: constants.RESPONSE_STATUS_SUCCESS,
-            user,
-            token,
-            error: ""
-        });
-    } catch (err) {
-        return res.status(422).send({
-            status: constants.RESPONSE_STATUS_ERROR,
-            error: err.message
-        });
-    }
+		await user.save();
+
+		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+		delete user.password;
+		res.send({
+			status: constants.RESPONSE_STATUS_SUCCESS,
+			user,
+			token,
+			error: '',
+		});
+	} catch (err) {
+		return res.status(422).send({
+			status: constants.RESPONSE_STATUS_ERROR,
+			error: err.message,
+		});
+	}
 });
 
 router.post('/signin', async (req, res) => {
-    
-    try {
-        const { userName, password } = req.body;
+	try {
+		const { userName, password } = req.body;
 
-        if (!userName || !password) {
-            return res.status(422).send({
-                status: constants.RESPONSE_STATUS_ERROR,
-                error: constants.AUTH_RESPONSE.ERROR_INVALID_CREDENTIALS
-            });
-        }
+		if (!userName || !password) {
+			return res.status(422).send({
+				status: constants.RESPONSE_STATUS_ERROR,
+				error: constants.AUTH_RESPONSE.ERROR_INVALID_CREDENTIALS,
+			});
+		}
 
-        const user = await User.findOne({ userName });
-        if (!user) {
-            return res.send({
-                status: constants.RESPONSE_STATUS_ERROR,
-                error: constants.AUTH_RESPONSE.ERROR_INVALID_CREDENTIALS
-            });
-        }
+		const user = await User.findOne({ userName });
+		if (!user) {
+			return res.send({
+				status: constants.RESPONSE_STATUS_ERROR,
+				error: constants.AUTH_RESPONSE.ERROR_INVALID_CREDENTIALS,
+			});
+		}
 
-        try {
-            await user.comparePasswords(password);
-            const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
-            user.password = null;
-            console.log(user);
-            res.send({
-                status: constants.RESPONSE_STATUS_SUCCESS,
-                user,
-                token,
-                error: ""
-            });
-        } catch (err) {
-            return res.send({
-                status: constants.RESPONSE_STATUS_ERROR,
-                error: constants.AUTH_RESPONSE.ERROR_INVALID_CREDENTIALS
-            });
-        }
-
-    } catch (err) {
-        return res.status(422).send({
-            status: constants.RESPONSE_STATUS_ERROR,
-            error: err
-        });
-    }
+		try {
+			await user.comparePasswords(password);
+			const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY');
+			user.password = null;
+			console.log(user);
+			res.send({
+				status: constants.RESPONSE_STATUS_SUCCESS,
+				user,
+				token,
+				error: '',
+			});
+		} catch (err) {
+			return res.send({
+				status: constants.RESPONSE_STATUS_ERROR,
+				error: constants.AUTH_RESPONSE.ERROR_INVALID_CREDENTIALS,
+			});
+		}
+	} catch (err) {
+		return res.status(422).send({
+			status: constants.RESPONSE_STATUS_ERROR,
+			error: err,
+		});
+	}
 });
 
 router.get('/test', (req, res) => {
-    return res.send({
-        nombre: "Mauricio"
-    });
-})
+	return res.send({
+		nombre: 'Mauricio',
+	});
+});
 
 module.exports = router;
--- src/routes/movementsRoutes.js
+++ src/routes/movementsRoutes.js
@@ -1,46 +1,46 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const requierAuth = require('../middlewares/requireAuth')
+const requierAuth = require('../middlewares/requireAuth');
 const Movements = mongoose.model('Movements');
 
 var constants = require('../models/Constants');
 
 const router = express.Router();
 
-router.use(requierAuth)
+router.use(requierAuth);
 
 router.get('/movements', async (req, res) => {
-    try {
-        const movements = await Movements.find({ userId: req.user._id });
-        res.send(movements);
-    } catch (err) {
-        return res.status(422).send({
-            status: constants.RESPONSE_STATUS_ERROR,
-            error: err.message
-        });
-    }
+	try {
+		const movements = await Movements.find({ userId: req.user._id });
+		res.send({
+			status: constants.RESPONSE_STATUS_SUCCESS,
+			movements,
+			error: '',
+		});
+	} catch (err) {
+		return res.status(422).send({
+			status: constants.RESPONSE_STATUS_ERROR,
+			error: err.message,
+		});
+	}
 });
 
 router.post('/movement', async (req, res) => {
-    try {
-        const movement = new Movements({
-            holder,
-            reference,
-            amount,
-            isDebit
-        } = req.body);
+	try {
+		const movement = new Movements(
+			({ holder, reference, amount, isDebit } = req.body)
+		);
 
-        movement.userId = req.user._id;
+		movement.userId = req.user._id;
 
-        await movement.save();
-        res.send(200, movement);
-    } catch (err) {
-        return res.status(422).send({
-            status: constants.RESPONSE_STATUS_ERROR,
-            error: err.message
-        });
-    }
+		await movement.save();
+		res.send(200, movement);
+	} catch (err) {
+		return res.status(422).send({
+			status: constants.RESPONSE_STATUS_ERROR,
+			error: err.message,
+		});
+	}
 });
-
 
 module.exports = router;
